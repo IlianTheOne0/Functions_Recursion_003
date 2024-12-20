ef002d142d889f171f324c3a5e1066dc7175ac8c
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-12-21 00:28:57 +0200

    Completed Task 2

--- main.cpp
+++ main.cpp
@@ -5,9 +5,46 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+template<typename TValue>
+void delete_value(TValue*& value)
+{
+    delete value;
+    value = nullptr;
+}
+
+template<typename TReturn, typename TValue>
+TReturn* get_value(TValue message)
+{
+    TReturn result;
+
+    cout << message;
+    cin >> result;
+
+    return new TReturn(result);
+}
+
+template<typename TReturn, typename TValue>
+TReturn display(TValue value)
+{
+    if (value == 0)
+        return 1;
+
+    cout << "*";
+
+    TValue new_value = value - 1;
+    return display<TReturn, TValue>(new_value);
+}
+
 int main()
 {
+    int* number = get_value<int, const char[]>("Enter the number of stars you want to display: ");
 
-	char ch = _getch();
-	return 0;
+    cout << "Result: ";
+    display<int, int>(*number);
+    cout << endl;
+
+    delete_value(number);
+
+    char ch = _getch();
+    return 0;
 }
