80e902eb4004e19882ca349e89f02703377445b5
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-12-21 02:01:14 +0200

    Completed Task 3

--- main.cpp
+++ main.cpp
@@ -5,9 +5,55 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+template<typename TValue>
+void delete_value(TValue*& value)
+{
+    delete value;
+    value = nullptr;
+}
+
+template<typename TReturn, typename TValue>
+TReturn* get_value(TValue message)
+{
+    TReturn result;
+
+    cout << message;
+    cin >> result;
+
+    return new TReturn(result);
+}
+
+template<typename TReturn, typename TValue>
+TReturn display(TValue* number1, TValue* number2, TReturn result)
+{
+    result += *number1;
+    if (*number1 == *number2)
+    {
+        return result;
+    }
+
+    (*number1)++;
+    return display<TReturn, TValue>(number1, number2, result);
+}
+
+
 int main()
 {
+    int* number1 = get_value<int, const char[]>("Enter the first number: ");
+    int* number2 = get_value<int, const char[]>("Enter the second number: ");
 
-	char ch = _getch();
-	return 0;
+    if (*number2 < *number1)
+    {
+        int temp = *number1;
+        *number1 = *number2;
+        *number2 = temp;
+    }
+    int result{};
+    cout << endl << "Result: " << display<int, int>(number1, number2, result) << endl;
+
+    delete_value(number1);
+    delete_value(number2);
+
+    char ch = _getch();
+    return 0;
 }
