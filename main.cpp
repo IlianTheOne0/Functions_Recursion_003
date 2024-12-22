5123f4563528241fd4f931ce0ea03312428c6f6a
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-12-22 16:41:58 +0200

    Completed Task 3

--- main.cpp
+++ main.cpp
@@ -1,169 +1,58 @@
 #include <iostream>
 #include <conio.h>
-#include <random>
-#include <Windows.h>
-#include <iomanip>
 
 using std::cout;
 using std::endl;
 using std::cin;
-using std::random_device;
-using std::mt19937;
-using std::uniform_int_distribution;
-using std::setw;
 
-template<typename TValueL>
-void delete_value(TValueL*& value, char indicator)
+template<typename TValue>
+void delete_value(TValue*& value)
 {
     delete value;
     value = nullptr;
 }
-template<typename TValueL>
-void delete_value(TValueL*& arr, bool indicator)
-{
-    delete[] arr;
-    arr = nullptr;
-}
 
-template<typename TValue>
-void init_arr(TValue* arr, int* size)
+template<typename TReturn, typename TValue>
+TReturn* get_value(TValue message)
 {
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<int> dist(0, 99);
+    TReturn result;
 
-    for (int i = 0; i < *size; i++)
-    {
-        arr[i] = dist(gen);
-    }
-}
-
-template<typename TValueL, typename TValueR>
-void print_arr(TValueL* arr, int* size, TValueR* message)
-{
     cout << message;
-    for (int i = 0; i < *size; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+    cin >> result;
+
+    return new TReturn(result);
 }
 
-template<typename TValue>
-void calc(TValue* arr, int i, int j = 1)
+template<typename TReturn, typename TValue>
+TReturn display(TValue* number1, TValue* number2, TReturn result)
 {
-    if (j == 1)
+    result += *number1;
+    if (*number1 == *number2)
     {
-        cout << setw(2) << arr[i] << " == ";
+        return result;
     }
 
-    if (j < 10)
-    {
-        cout << setw(2) << arr[j + i] << " | ";
-        arr[i] += arr[j + i];
-        calc<TValue>(arr, i, j + 1);
-    }
-    else
-    {
-        cout << "== " << arr[i] << endl;
-    }
+    (*number1)++;
+    return display<TReturn, TValue>(number1, number2, result);
 }
 
-template<typename TValue>
-TValue rangefinder(TValue* arr, int* size, int i)
-{
-    if (i == (*size - 10))
-    {
-        return *arr;
-    }
-
-    /*
-    cout << setw(2) << arr[i] << " == ";
-    for (int j = 1; j < 10; j++)
-    {
-        cout << setw(2) << arr[j + i] << " | ";
-
-        arr[i] += arr[j + i];
-    }
-    cout << "== " << arr[i] << endl;
-    */
-
-    calc<int>(arr, i);
-
-    return rangefinder<TValue>(arr, size, i + 1);
-}
-
-template<typename TValue>
-void overwriting(TValue* arr, TValue* arr0, int* size)
-{
-    for (int i = 0; i < *size; ++i) {
-        arr0[i] = arr[i];
-    }
-}
-
-template<typename TValue>
-void sorting(TValue* arr, int* size)
-{
-    for (int i = 1; i < *size; ++i) {
-        int key = arr[i];
-        int j = i - 1;
-
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
-}
-
-template<typename TValue>
-int index_searcher(TValue* arr, TValue* arr0, int* size)
-{
-    int number = arr[10];
-    int index;
-
-    for (int i = 0; i < *size; i++)
-    {
-        if (arr0[i] == number)
-        {
-            index = i;
-        }
-    }
-
-    return index;
-}
 
 int main()
 {
-    int* size = new int{ 100 };
-    int* arr = new int[*size];
-    init_arr<int>(arr, size);
-    print_arr<int, const char>(arr, size, "Array: ");
+    int* number1 = get_value<int, const char[]>("Enter the first number: ");
+    int* number2 = get_value<int, const char[]>("Enter the second number: ");
 
+    if (*number2 < *number1)
+    {
+        int temp = *number1;
+        *number1 = *number2;
+        *number2 = temp;
+    }
+    int result{};
+    cout << endl << "Result: " << display<int, int>(number1, number2, result) << endl;
 
-    cout << endl << endl << endl << "Step 1. Calculations:" << endl;
-    int i = 0;
-    *arr = rangefinder<int>(arr, size, i);
-    cout << endl;
-    print_arr(arr, size, "Intermediate result: ");
-
-
-
-    cout << endl << endl << endl << "Step 2. Sorting:" << endl;
-    int* original_arr = new int[*size];
-    overwriting<int>(arr, original_arr, size);
-    sorting<int>(arr, size);
-    print_arr(arr, size, "Sorted array: ");
-    cout << endl;
-
-
-    cout << endl << endl << endl << "Step 3. Find the position where the sequence of 10 numbers starts:" << endl;
-    i = index_searcher(arr, original_arr, size);
-    cout << "Thus, the sequence of digits whose sum is minimal began with " << i << " of the index" << endl;
-
-    delete_value<int>(arr, false);
-    delete_value<int>(original_arr, false);
-    delete_value<int>(size, ' ');
+    delete_value(number1);
+    delete_value(number2);
 
     char ch = _getch();
     return 0;
