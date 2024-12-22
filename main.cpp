b121afc2a5ae7da2f09424430c8774d7d6eec7a3
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-12-22 16:36:40 +0200

    Completed Task 3

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <conio.h>
 #include <random>
 #include <Windows.h>
+#include <iomanip>
 
 using std::cout;
 using std::endl;
@@ -9,6 +10,7 @@ using std::cin;
 using std::random_device;
 using std::mt19937;
 using std::uniform_int_distribution;
+using std::setw;
 
 template<typename TValue>
 void delete_value(TValue*& value)
@@ -54,6 +56,89 @@ void print_arr(TValueL* arr, int* size, TValueR* message)
     cout << endl;
 }
 
+template<typename TValue>
+void calc(TValue* arr, int i, int j = 1)
+{
+    if (j == 1)
+    {
+        cout << setw(2) << arr[i] << " == ";
+    }
+
+    if (j < 10)
+    {
+        cout << setw(2) << arr[j + i] << " | ";
+        arr[i] += arr[j + i];
+        calc<TValue>(arr, i, j + 1);
+    }
+    else
+    {
+        cout << "== " << arr[i] << endl;
+    }
+}
+
+template<typename TValue>
+TValue rangefinder(TValue* arr, int* size, int i)
+{
+    if (i == (*size - 10))
+    {
+        return *arr;
+    }
+
+    /*
+    cout << setw(2) << arr[i] << " == ";
+    for (int j = 1; j < 10; j++)
+    {
+        cout << setw(2) << arr[j + i] << " | ";
+
+        arr[i] += arr[j + i];
+    }
+    cout << "== " << arr[i] << endl;
+    */
+
+    calc<int>(arr, i);
+
+    return rangefinder<TValue>(arr, size, i + 1);
+}
+
+template<typename TValue>
+void overwriting(TValue* arr, TValue* arr0, int* size)
+{
+    for (int i = 0; i < *size; ++i) {
+        arr0[i] = arr[i];
+    }
+}
+
+template<typename TValue>
+void sorting(TValue* arr, int* size)
+{
+    for (int i = 1; i < *size; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+template<typename TValue>
+int index_searcher(TValue* arr, TValue* arr0, int* size)
+{
+    int number = arr[10];
+    int index;
+
+    for (int i = 0; i < *size; i++)
+    {
+        if (arr0[i] == number)
+        {
+            index = i;
+        }
+    }
+
+    return index;
+}
 
 int main()
 {
@@ -62,7 +147,29 @@ int main()
     init_arr<int>(arr, size);
     print_arr<int, const char>(arr, size, "Array: ");
 
+
+    cout << endl << endl << endl << "Step 1. Calculations:" << endl;
+    int i = 0;
+    *arr = rangefinder<int>(arr, size, i);
+    cout << endl;
+    print_arr(arr, size, "Intermediate result: ");
+
+
+
+    cout << endl << endl << endl << "Step 2. Sorting:" << endl;
+    int* original_arr = new int[*size];
+    overwriting<int>(arr, original_arr, size);
+    sorting<int>(arr, size);
+    print_arr(arr, size, "Sorted array: ");
+    cout << endl;
+
+
+    cout << endl << endl << endl << "Step 3. Find the position where the sequence of 10 numbers starts:" << endl;
+    i = index_searcher(arr, original_arr, size);
+    cout << "Thus, the sequence of digits whose sum is minimal began with " << i << " of the index" << endl;
+
     delete_value<int>(arr, false);
+    delete_value<int>(original_arr, false);
     delete_value<int>(size, ' ');
 
     char ch = _getch();
