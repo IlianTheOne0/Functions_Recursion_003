e301ec156bca6d242fe5a109188385678a4668d3
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-12-23 20:04:32 +0200

    mtp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,33 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+template<typename TValue>
+void delete_value(TValue*& value)
+{
+    delete value;
+    value = nullptr;
+}
+
+template<typename TReturn, typename TValue>
+TReturn* get_value(TValue message)
+{
+    TReturn result;
+
+    cout << message;
+    cin >> result;
+
+    return new TReturn(result);
+}
+
 int main()
 {
+    int* number1 = get_value<int, const char[]>("Enter the first number: ");
+    int* number2 = get_value<int, const char[]>("Enter the second number: ");
 
-	char ch = _getch();
-	return 0;
+
+    delete_value(number1);
+    delete_value(number2);
+
+    char ch = _getch();
+    return 0;
 }
