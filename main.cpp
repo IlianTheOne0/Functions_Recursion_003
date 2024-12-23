566a08ebe52add2ddfdb81210e216b59e8485cb0
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-12-23 20:13:49 +0200

    Completed Task 2

--- main.cpp
+++ main.cpp
@@ -23,14 +23,29 @@ TReturn* get_value(TValue message)
     return new TReturn(result);
 }
 
+template<typename TReturn, typename TValue>
+TReturn GCD(TValue* number0, TValue* number1)
+{
+    if (*number1 == 0)
+    {
+        return *number0;
+    }
+
+    TReturn result = *number0 % *number1;
+
+    return GCD<TReturn, TValue>(number1, &result);
+}
+
+
 int main()
 {
-    int* number1 = get_value<int, const char[]>("Enter the first number: ");
-    int* number2 = get_value<int, const char[]>("Enter the second number: ");
+    int* number0 = get_value<int, const char[]>("Enter the first number: ");
+    int* number1 = get_value<int, const char[]>("Enter the second number: ");
 
+    cout << endl << endl << "The greatest common divisor: " << GCD<int, int>(number0, number1) << endl;
 
+    delete_value(number0);
     delete_value(number1);
-    delete_value(number2);
 
     char ch = _getch();
     return 0;
